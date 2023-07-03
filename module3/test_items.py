eb7690ca63fe0490f200e9f35d5125a189128b8d
sonya <cjabzdbitp@mail.ru>
2023-07-03 22:47:00 +0100

    test - check add button

---
+++ module3/test_items.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+
+def test_add_to_cart_button_exists(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(30)  # Добавлено ожидание для визуальной проверки языка страницы
+    add_to_cart_button = browser.find_element(By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary '
+                                                               'btn-add-to-basket"]')
+    assert add_to_cart_button, "Add to cart button is not found"
